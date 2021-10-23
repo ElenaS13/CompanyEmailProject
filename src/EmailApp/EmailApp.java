2b250ecf5417753214d5b14813f6f5a22dbde1d9
ElenaS1981 <elenasidyakina@gmail.com>
2021-10-22 21:30:47 -0700

    complete program for email

---
+++ src/EmailApp/EmailApp.java
@@ -0,0 +1,13 @@
+package EmailApp;
+
+public class EmailApp {
+    public static void main(String[] args) {
+
+        Email em1 = new Email("ES", "ES");
+        System.out.println( em1.showInfo());
+
+
+
+
+    }
+}
